d6fbfdc0c562f1efeb3d9f9f74576673c1851991
anrefranciscus <anrefranciscus@gmail.com>
2024-07-13 03:45:11 +0700

    add reusable component input and layouting form login

---
+++ frontend-vue/src/components/BaseInputText.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <label :for="id" class="block text-sm font-medium text-gray-700">{{
+      label
+    }}</label>
+    <input
+      :id="id"
+      :type="type"
+      :value="value"
+      @input="handleInput"
+      :placeholder="placeholder"
+      class="w-full mt-1 p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+      :required="required"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseInputText",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      type: [String, Number],
+      default: "",
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleInput(event: Event) {
+      const target = event.target as HTMLInputElement;
+      this.$emit("update:modelValue", target.value);
+    },
+  },
+});
+</script>
--- frontend-vue/src/views/LoginPage.vue
+++ frontend-vue/src/views/LoginPage.vue
@@ -1,28 +1,47 @@
 <template>
-  <form @submit.prevent="login" class="flex flex-col">
-    <input
-      type="text"
-      v-model="formData.username"
-      placeholder="Username"
-      required
-    />
-    <input
-      type="password"
-      v-model="formData.password"
-      placeholder="Password"
-      required
-    />
-    <button type="submit">Login</button>
-  </form>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+      <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
+      <form @submit.prevent="login" class="space-y-6">
+        <BaseInputText
+          id="username"
+          label="Username"
+          v-model="formData.username"
+          placeholder="Enter your username"
+          required
+        />
+        <BaseInputText
+          id="password"
+          label="Password"
+          type="password"
+          v-model="formData.password"
+          placeholder="Enter your username"
+          required
+        />
+        <div>
+          <button
+            type="submit"
+            class="w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import type { UserLoginPayload } from "@/types";
 import { AuthService } from "@/services/AuthService";
 import { useRouter } from "vue-router";
+import BaseInputText from "@/components/BaseInputText.vue";
 
 export default defineComponent({
   name: "LoginPage",
+  components: {
+    BaseInputText,
+  },
   setup() {
     const formData = ref<UserLoginPayload>({
       username: "",
